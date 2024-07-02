18826fb91ed14445c1aa65e7cb254ede5ba07ee1
Raghav Sood <r@raghavsood.com>
2024-07-02 17:33:01 +0800

    db: create god view

---
+++ storage/sqlite/migrations/20240702091945_create_god_view.sql
@@ -0,0 +1,33 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE VIEW god_view AS
+SELECT
+  s.name AS series_name,
+  s.slug AS series_id,
+  json_group_array(DISTINCT(c.name)) AS creators,
+  i.sku AS item_id,
+  i.serial,
+  a.address,
+  SUM(op.value) AS total_value,
+  MIN(op.block_time) AS first_active,
+  MAX(op.spending_block_time) AS redeemed_on
+FROM
+  series s
+JOIN
+  series_creators sc ON s.slug = sc.series_slug
+JOIN
+  creators c ON sc.creator_slug = c.slug
+JOIN
+  items i ON s.slug = i.series_slug
+LEFT JOIN
+  addresses a ON i.sku = a.sku
+LEFT JOIN
+  outpoints op ON a.script = op.script
+GROUP BY
+  s.name, s.slug, i.sku, i.serial, a.address;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP VIEW god_view;
+-- +goose StatementEnd
