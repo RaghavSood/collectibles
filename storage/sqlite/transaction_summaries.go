daec0ede099b68932bc377ce2d8a36bdeef81bd4
Raghav Sood <r@raghavsood.com>
2024-08-06 16:48:04 +0800

    storage/transaction: query from computed table instead of view

--- storage/sqlite/transaction_summaries.go
+++ storage/sqlite/transaction_summaries.go
@@ -7,7 +7,7 @@ import (
 )
 
 func (d *SqliteBackend) TransactionSummariesByItem(sku string) ([]types.Transaction, error) {
-	query := `SELECT txid, vout, vin, original_txid, value, block_height, block_time, transaction_type, sku, series_slug, serial, name FROM transaction_view WHERE sku = ? ORDER BY block_height DESC;`
+	query := `SELECT txid, vout, vin, original_txid, value, block_height, block_time, transaction_type, sku, series_slug, serial, name FROM transaction_summary_c WHERE sku = ? ORDER BY block_height DESC;`
 
 	rows, err := d.db.Query(query, sku)
 	if err != nil {
@@ -18,7 +18,7 @@ func (d *SqliteBackend) TransactionSummariesByItem(sku string) ([]types.Transact
 }
 
 func (d *SqliteBackend) TransactionSummariesBySeries(slug string, limit int) ([]types.Transaction, error) {
-	query := `SELECT txid, vout, vin, original_txid, value, block_height, block_time, transaction_type, sku, series_slug, serial, name FROM transaction_view WHERE series_slug = ? ORDER BY block_height DESC LIMIT ?;`
+	query := `SELECT txid, vout, vin, original_txid, value, block_height, block_time, transaction_type, sku, series_slug, serial, name FROM transaction_summary_c WHERE series_slug = ? ORDER BY block_height DESC LIMIT ?;`
 
 	rows, err := d.db.Query(query, slug, limit)
 	if err != nil {
@@ -29,7 +29,7 @@ func (d *SqliteBackend) TransactionSummariesBySeries(slug string, limit int) ([]
 }
 
 func (d *SqliteBackend) TransactionSummariesByCreator(slug string, limit int) ([]types.Transaction, error) {
-	query := `SELECT txid, vout, vin, original_txid, value, block_height, block_time, transaction_type, sku, series_slug, serial, name FROM transaction_view WHERE series_slug IN (SELECT series_slug FROM series_creators WHERE creator_slug = ?) ORDER BY block_height DESC LIMIT ?;`
+	query := `SELECT txid, vout, vin, original_txid, value, block_height, block_time, transaction_type, sku, series_slug, serial, name FROM transaction_summary_c WHERE series_slug IN (SELECT series_slug FROM series_creators WHERE creator_slug = ?) ORDER BY block_height DESC LIMIT ?;`
 
 	rows, err := d.db.Query(query, slug, limit)
 	if err != nil {
@@ -40,7 +40,7 @@ func (d *SqliteBackend) TransactionSummariesByCreator(slug string, limit int) ([
 }
 
 func (d *SqliteBackend) TransactionSummaries(limit int) ([]types.Transaction, error) {
-	query := `SELECT txid, vout, vin, original_txid, value, block_height, block_time, transaction_type, sku, series_slug, serial, name FROM transaction_view ORDER BY block_height DESC LIMIT ?;`
+	query := `SELECT txid, vout, vin, original_txid, value, block_height, block_time, transaction_type, sku, series_slug, serial, name FROM transaction_summary_c ORDER BY block_height DESC LIMIT ?;`
 
 	rows, err := d.db.Query(query, limit)
 	if err != nil {
